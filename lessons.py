18f6d592866e68c169b90d75eebd126036c92c58
Your Name <you@example.com>
2021-02-05 11:56:58 +0300

    eremens

--- lessons.py
+++ lessons.py
@@ -1,2 +1,13 @@
 print ("Hello world line 1")
 print ("Hello world line 2")
+
+#------------------------------
+# Переменные
+#------------------------------
+
+a=7 ; b=3 ; print (a+b)
+f_name = "Alexey" ; l_name = "Ryabtsev" ; print ("Creator: "+ f_name + ' ' + l_name )
+
+#------------------------------
+# Переменные
+#------------------------------
