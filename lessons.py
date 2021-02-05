af3350956476664e90d24e65578184223ee2dad9
Your Name <you@example.com>
2021-02-05 12:11:38 +0300

    new

--- lessons.py
+++ lessons.py
@@ -6,8 +6,21 @@ print ("Hello world line 2")
 #------------------------------
 
 a=7 ; b=3 ; print (a+b)
-f_name = "Alexey" ; l_name = "Ryabtsev" ; print ("Creator: "+ f_name + ' ' + l_name )
+f_name = "Alexey" ; l_name = "Ryabtsev" ; print ("Creator: "+ f_name + ' ' + l_name)
 
 #------------------------------
-# Переменные
+# Строки
 #------------------------------
+
+name = "КрИвОй ТеКсТ"
+
+print (name.title())
+print (name.upper())
+print (name.lower())
+
+print  ("Какая-то строка\nЕще одна строка\n\nЧерез строку\n")
+print  ("Какая-то строка\n\tЕще одна строка\n\tt значит таб \n")
+print ("Lower name: " + f_name.lower())
+#.rstrip() - стирает пробелы справа
+#.lstrip() - стирает пробелы справа
+#.strip() - стирает пробелы с двух сторон или ставим символы в ковычках какие подрезать .strip("")
